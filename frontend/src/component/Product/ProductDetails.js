ba59964b7cd112e97ee2e2ef9dc23339049f1096
Abdullah9862873 <ag9862873@gmail.com>
2023-01-16 23:37:58 +0500

    initial commit

---
+++ frontend/src/component/Product/ProductDetails.js
@@ -0,0 +1,125 @@
+import React, { Fragment, useEffect, useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
+import { useParams } from "react-router-dom";
+import MetaData from "../layout/MetaData";
+import ReactStars from "react-rating-stars-component";
+import Loader from "../layout/Loader/Loader";
+import { addItemsToCart } from "../../actions/cartAction";
+import { useAlert } from "react-alert";
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const alert = useAlert();
+
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
+
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (1 >= quantity) return;
+
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+
+  const addToCartHandler = () => {
+    dispatch(addItemsToCart(id, quantity));
+    alert.success("Item Added To Cart");
+  };
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title={`${product.name} -- ECOMMERCE`} />
+          <div className="ProductDetails">
+            <div className="firstDiv">
+              <Carousel stopAutoPlayOnHover className="myCarousel">
+                {product.images &&
+                  product.images.map((item, i) => (
+                    <img
+                      className="CarouselImage"
+                      key={i}
+                      src={item.url}
+                      alt={`${i} Slide`}
+                    />
+                  ))}
+              </Carousel>
+            </div>
+
+            <div className="secondDiv">
+              <div className="detailsBlock-1">
+                <h2>{product.name}</h2>
+                <p>Product # {product._id}</p>
+              </div>
+              <div className="detailsBlock-2">
+                <ReactStars {...options} />
+                <span className="detailsBlock-2-span">
+                  {" "}
+                  ({product.numOfReviews} Reviews)
+                </span>
+              </div>
+              <div className="detailsBlock-3">
+                <h1>{`Rs ${product.price}`}</h1>
+                <div className="detailsBlock-3-1">
+                  <div className="detailsBlock-3-1-1">
+                    <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly type="number" value={quantity} />
+                    <button onClick={increaseQuantity}>+</button>
+                  </div>
+                  <button
+                    disabled={product.Stock < 1 ? true : false}
+                    onClick={addToCartHandler}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+
+                <p>
+                  Status:
+                  <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                  </b>
+                </p>
+              </div>
+
+              <div className="detailsBlock-4">
+                Description : <p>{product.description}</p>
+              </div>
+
+              <button className="submitReview">Submit Review</button>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default ProductDetails;
